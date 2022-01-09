1e937d8fc3d939263e98a48d036193f525017d2f
yen-nhi <yenhinguyen.92@gmail.com>
2022-01-10 01:52:39 +0700

    Update app urls

---
+++ memories/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("logout", views.logout_view, name="logout"),
+    path("new-memory", views.new_memory, name="new_memory"),
+]
