a79177791ee4ece457dd188c409b9fbe4f974645
yen-nhi <yenhinguyen.92@gmail.com>
2022-01-10 01:43:37 +0700

    Create Profile and Memory model

--- memories/models.py
+++ memories/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.base import Model
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name='profile_user')
+    uid = models.CharField(max_length=35)
+    avatar = models.CharField(max_length=300)
+
+
+class Memory(models.Model):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name='memory_user')
+    name = models.CharField(max_length=150)
+    comment = models.CharField(max_length=150)
+    time = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=35)
+    def serialize(self):
+        return {
+            'username': self.user.username,
+            'name': self.name,
+            'comment': self.comment,
+            'location': self.location,
+        }
