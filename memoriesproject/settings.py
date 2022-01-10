c4480d62d052f86e02c54ced056f87e20a71401e
yen-nhi <yenhinguyen.92@gmail.com>
2022-01-11 04:01:20 +0700

    update

--- memoriesproject/settings.py
+++ memoriesproject/settings.py
@@ -12,7 +12,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+# import environ
 
+# env = environ.Env()
+# # reading .env file
+# environ.Env.read_env()
+
+#Django Secret key
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
